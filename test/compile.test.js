3d88e2bcab9d789457939ccba5d1aed25d20ea33
Axes <whxaxes@gmail.com>
2018-11-17 01:46:56 +0800

    feat: add async

--- test/compile.test.js
+++ test/compile.test.js
@@ -2,12 +2,12 @@ const compiler = require('../lib/compile/compile');
 
 describe('lib#compile', () => {
   it('should run without error if ast.root is not exist', () => {
-    compiler({});
-    compiler({ root: [] });
+    compiler({}, undefined, {});
+    compiler({ root: [] }, undefined, {});
   });
 
   it('should run without error if astNode has error type', () => {
-    compiler({ root: [{ type: 4 }] });
-    compiler({ root: [{ type: 1 }] });
+    compiler({ root: [{ type: 4 }] }, undefined, {});
+    compiler({ root: [{ type: 1 }] }, undefined, {});
   });
 });
